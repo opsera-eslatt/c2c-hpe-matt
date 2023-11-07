eabe4ee1103ddb5782f787feebad304d4579b2bd
Matt Smitton <m@smitton.io>
2023-11-07 15:51:45 -0500

    matt to splatt

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fulfillment - Code to Cloud - HPE Matt
+          Fulfillment - Code to Cloud - HPE Splatt
         </a>
       </header>
     </div>
